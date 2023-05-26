c4a35730e2b7910e89f9291b4e98ec4de10f771f
Matt Groff <matt@umbrage.com>
2023-05-26 17:32:39 -0400

    Duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :scraping_job_histories
-  resources :scraping_jobs
   resources :users
   resources :scraping_jobs do
     get 'latest_content', on: :member
   end
+  resources :scraping_job_histories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
