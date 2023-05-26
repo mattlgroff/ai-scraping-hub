c65c3b87ee86117055db4b397e96a9dd2e791392
Matt Groff <matt@umbrage.com>
2023-05-26 01:23:48 -0400

    Add root homepage placeholder

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -9,5 +9,8 @@ fi
 echo "Migrating data or creating database if not exists..."
 bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
 
+echo "Building Tailwind CSS..."
+bundle exec rake tailwindcss:build
+
 echo "Starting server..."
 bundle exec rails s -b 0.0.0.0
